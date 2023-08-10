b7f63517bf4241d4db032b494f6784c48de68290
Joel Jarro <rodrigoquiroz308@gmail.com>
2023-08-10 10:48:40 -0400

    add modals to note

--- Frontend/src/components/Note.tsx
+++ Frontend/src/components/Note.tsx
@@ -1,8 +1,15 @@
 import { Archive, DeleteCircle, Edit } from "iconoir-react";
 import React from "react";
 import Button from "./Button";
+import { ConfirmModal } from "./ConfirmModal";
+import Modal from "./Modal";
+import NoteForm from "./NoteForm";
 
 export const Note = () => {
+  const [isOpenDelete, setIsOpenDelete] = React.useState(false);
+  const [isOpenArchive, setIsOpenArchive] = React.useState(false);
+  const [isOpenEdit, setIsOpenEdit] = React.useState(false);
+
   return (
     <div className="flex max-w-sm rounded overflow-hidden shadow-lg">
       <div className="px-6 py-4">
@@ -14,19 +21,42 @@ export const Note = () => {
         </p>
       </div>
       <div className="px-2 pt-4 pb-2 self-center">
-        <Button theme="primary">
+        <Button onClick={() => setIsOpenEdit(true)} theme="primary">
           <Edit />
           Edit
         </Button>
-        <Button theme="primary">
+        <Button onClick={() => setIsOpenDelete(true)} theme="primary">
           <DeleteCircle />
           Delete
         </Button>
-        <Button theme="secondary">
+        <Button onClick={() => setIsOpenArchive(true)} theme="secondary">
           <Archive />
           Archive
         </Button>
       </div>
+      <ConfirmModal
+        isOpen={isOpenDelete}
+        setOpen={setIsOpenDelete}
+        onClose={() => setIsOpenDelete(false)}
+        onClick={() => console.log("Delete")}
+      >
+        Are you sure you want to delete this note?
+      </ConfirmModal>
+      <ConfirmModal
+        isOpen={isOpenArchive}
+        setOpen={setIsOpenArchive}
+        onClose={() => setIsOpenArchive(false)}
+        onClick={() => console.log("Archive")}
+      >
+        Are you sure you want to archive this note?
+      </ConfirmModal>
+      <Modal
+        isOpen={isOpenEdit}
+        setOpen={setIsOpenEdit}
+        onClose={() => setIsOpenEdit(false)}
+      >
+        <NoteForm setModalOpen={setIsOpenEdit} />
+      </Modal>
     </div>
   );
 };
