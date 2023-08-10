ca6646f826d8e8796b04e011e33d64b40f406b2a
Joel Jarro <rodrigoquiroz308@gmail.com>
2023-08-10 09:10:13 -0400

    add note component

--- Frontend/src/pages/Home.tsx
+++ Frontend/src/pages/Home.tsx
@@ -1,5 +1,6 @@
 import { Archive, DeleteCircle, Edit } from "iconoir-react";
 import React from "react";
+import { Note } from "../components/Note";
 
 export const Home = () => {
   return (
@@ -9,36 +10,13 @@ export const Home = () => {
         KnockNote is a simple note taking app
       </h2>
       <div className="flex flex-wrap">
-        <div className="flex max-w-sm rounded overflow-hidden shadow-lg">
-          <div className="px-6 py-4">
-            <div className="font-bold text-xl mb-2">Note 1</div>
-            <p className="text-base line-clamp-3">
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-              Voluptatibus quia, nulla! Maiores et perferendis eaque,
-              exercitationem praesentium nihil.
-            </p>
-          </div>
-          <div className="px-6 pt-4 pb-2 self-center">
-            <button className="w-full flex bg-primary hover:opacity-70 rounded-full px-3 py-1 text-sm font-semibold text-white mr-2 mb-2">
-              <Edit />
-              Edit
-            </button>
-            <button
-              type="button"
-              className="w-full flex bg-primary hover:opacity-70 rounded-full px-3 py-1 text-sm font-semibold text-white mr-2 mb-2"
-            >
-              <DeleteCircle />
-              Delete
-            </button>
-            <button
-              type="button"
-              className="w-full flex bg-yellow-400 hover:bg-yellow-500 rounded-full px-3 py-1 text-sm font-semibold text-black mr-2 mb-2"
-            >
-              <Archive />
-              Archive
-            </button>
-          </div>
-        </div>
+        <Note />
+        <Note />
+        <Note />
+        <Note />
+        <Note />
+        <Note />
+
       </div>
     </>
   );
