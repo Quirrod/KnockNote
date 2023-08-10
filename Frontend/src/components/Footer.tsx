abdc469d808aff61a5048147af0b6d9d451180ba
Joel Jarro <rodrigoquiroz308@gmail.com>
2023-08-10 10:59:55 -0400

    fix footer name

--- Frontend/src/components/Footer.tsx
+++ Frontend/src/components/Footer.tsx
@@ -1,8 +1,17 @@
+import { Link } from "react-router-dom";
+
 export const Footer = () => {
   return (
     <footer className=" py-10 lg:px-72 w-full backdrop-blur-md">
       <div className="text-sm font-medium text-center leading-loose">
-        © 2023 KnockNote. Made with ❤️ by
+        © 2023 KnockNote. Made with ❤️ by  &nbsp;
+        <Link
+          className="text-primary"
+          to="https://www.linkedin.com/in/joel-jarro/"
+          target="_blank"
+        >
+          Quirrod
+        </Link>
       </div>
     </footer>
   );
