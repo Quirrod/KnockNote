4340cb0954cc13d046321b4a6884e578aa25329b
Joel Jarro <rodrigoquiroz308@gmail.com>
2023-08-10 22:41:04 -0400

    add burguer menu to navbar to responsive

--- Frontend/src/components/Navbar.tsx
+++ Frontend/src/components/Navbar.tsx
@@ -1,4 +1,5 @@
 import { Notes, Menu } from "iconoir-react";
+import { useState } from "react";
 import { NavLink } from "react-router-dom";
 
 export const Navbar = () => {
@@ -6,6 +7,8 @@ export const Navbar = () => {
     "px-3 py-2 rounded-md text-sm font-medium text-white bg-primary focus:outline-none focus:text-white focus:bg-secondary";
   const linkInactiveClass =
     "px-3 py-2 rounded-md text-sm font-medium text-gray-300 hover:text-white hover:bg-gray-700 focus:outline-none focus:text-white focus:bg-gray-700";
+
+  const [isOpen, setIsOpen] = useState(false);
 
   return (
     <header className="sm:flex sm:justify-between sm:items-center sm:py-3 w-full">
@@ -16,14 +19,41 @@ export const Navbar = () => {
         <div className="sm:hidden">
           <button
             type="button"
-            className="block text-gray-500 hover:text-white focus:text-white focus:outline-none"
+            className={`hover:text-white focus:text-white focus:outline-none
+            ${isOpen ? "text-white" : "text-gray-300"}
+            `}
           >
-            <Menu name="menu" />
+            <Menu
+              name="menu"
+              onClick={() => {
+                setIsOpen(!isOpen);
+              }}
+            />
           </button>
+          {isOpen && (
+            <div className="fixed top-10 right-0 flex flex-col shadow-2xl backdrop-blur-sm">
+              <NavLink
+                className={({ isActive }) =>
+                  isActive ? linkActiveClass : linkInactiveClass
+                }
+                to="/"
+              >
+                Home
+              </NavLink>
+              <NavLink
+                className={({ isActive }) =>
+                  isActive ? linkActiveClass : linkInactiveClass
+                }
+                to="/archived"
+              >
+                Archived Notes
+              </NavLink>
+            </div>
+          )}
         </div>
       </div>
 
-      <div className="hidden sm:block sm:ml-6">
+      <div className="hidden sm:block">
         <div className="flex space-x-4">
           <NavLink
             className={({ isActive }) =>
