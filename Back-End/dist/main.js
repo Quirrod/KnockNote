c9f74b63fd3a753d095da15a7f3d3155719b7dab
Joel Jarro <rodrigoquiroz308@gmail.com>
2023-08-11 02:10:13 -0400

    add try catch to connection backend

--- Back-End/dist/main.js
+++ Back-End/dist/main.js
@@ -3,10 +3,16 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const core_1 = require("@nestjs/core");
 const app_module_1 = require("./app.module");
 async function bootstrap() {
-    const app = await core_1.NestFactory.create(app_module_1.AppModule);
-    app.setGlobalPrefix('api');
-    app.enableCors();
-    await app.listen(3000);
+    try {
+        const PORT = process.env.PORT || 3000;
+        const app = await core_1.NestFactory.create(app_module_1.AppModule);
+        app.setGlobalPrefix('api');
+        app.enableCors();
+        await app.listen(PORT, async () => console.log(`Server has been started on PORT: ${PORT}! and at ${await app.getUrl()}`));
+    }
+    catch (error) {
+        console.log(error);
+    }
 }
 bootstrap();
 //# sourceMappingURL=main.js.map
--- Back-End/src/main.ts
+++ Back-End/src/main.ts
@@ -2,9 +2,14 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  app.setGlobalPrefix('api')
-  app.enableCors()
-  await app.listen(3000);
+  try {
+    const PORT = process.env.PORT || 3000;
+    const app = await NestFactory.create(AppModule);
+    app.setGlobalPrefix('api')
+    app.enableCors()
+    await app.listen(PORT, async () => console.log(`Server has been started on PORT: ${PORT}! and at ${await app.getUrl()}`));
+  } catch (error) {
+    console.log(error)
+  }
 }
 bootstrap();
